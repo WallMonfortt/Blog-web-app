01c91b78047ba39508070ac36a57c09318e94854
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2025-01-03 15:40:16 -0600

    main template and left navbar
    css, js,

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    const colors = [
+      '--primary-color',
+      '--info-color',
+      '--success-color',
+      '--warning-color',
+      '--danger-color'
+    ];
+  
+    $('.menu-hover-fill li').each(function() {
+      let randomColor = Math.floor(Math.random() * colors.length)
+      const colorVar = colors[randomColor];
+      const colorValue = getComputedStyle(document.documentElement).getPropertyValue(colorVar);
+      $(this).css('--menu-link-active-color', colorValue);
+    });
+  });
+  
